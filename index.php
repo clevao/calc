ffdadd7ac3e7ff72888db2be5027287db33838be
cleverson <cleverson090@gmail.com>
2015-03-12 12:35:24 -0300

    termino da funcionalidade de SOMA

--- index.php
+++ index.php
@@ -5,19 +5,41 @@
 	<body>
 
 <?php
+
+	$v1 = $v2 = '';
+	$op = '+';
+	$res = "";
+
+	if(isset($_POST["enviar"])){
+		$v1 = $_POST["valor1"];
+		$v2 = $_POST["valor2"];
+		$op = $_POST["operacao"];
+		
+		switch($op){
+			case "+":
+				$res = $v1 + $v2;
+				break;
+			default:
+				$res = "ERRO";
+				break;
+		}
+		
+	}
+	
+	$strRes = '';
+	if($res != '')
+		$strRes = " = $res";
 	
 	
 	$corpo = '<h1>CALCULADORA<h2>
 			<form method="POST">
-				<input type="text" name="valor1" value="" class="inputTexto" style="width: 80px;">
+				<input type="text" name="valor1" value="'.$v1.'" class="inputTexto" style="width: 80px;">
 				<input type="hidden" name="enviar" value="true">
-				<select name="operacao" class="inputTexto" style="width: 40px;">
-					<option value="+">MAIS</option>
-					<option value="-">MENOS</option>
-					<option value="*">VEZES</option>
-					<option value="/">Divisao</option>
+				<select name="operacao" class="inputTexto" style="width: 80px;">
+					<option value="+">MAIS</option>					
 				</select>
-				<input type="text" name="valor2" value="" class="inputTexto" style="width: 80px;">
+				<input type="text" name="valor2" value="'.$v2.'" class="inputTexto" style="width: 80px;">
+				'.$strRes.'
 				<br><br>
 				<input type="submit" name="SALVAR" value="CALCULAR" class="botaoEnviar">
 			</form>';

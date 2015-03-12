cfe59d3f834fb3b6de8da4d5def524bb39dcab60
cleverson <cleverson090@gmail.com>
2015-03-12 12:52:38 -0300

    Correção do bug da selecao da operação, quando mostra o resultado

--- index.php
+++ index.php
@@ -9,6 +9,8 @@
 	$v1 = $v2 = '';
 	$op = '+';
 	$res = "";
+	
+	$arrayOp = array("+"=>"","-"=>"","*"=>"","/"=>"");
 
 	if(isset($_POST["enviar"])){
 		$v1 = $_POST["valor1"];
@@ -27,20 +29,21 @@
 				break;
 		}
 		
+		$arrayOp[$op] = 'selected';
+			
 	}
 	
 	$strRes = '';
 	if($res != '')
 		$strRes = " = $res";
 	
-	
 	$corpo = '<h1>CALCULADORA<h2>
 			<form method="POST">
 				<input type="text" name="valor1" value="'.$v1.'" class="inputTexto" style="width: 80px;">
 				<input type="hidden" name="enviar" value="true">
 				<select name="operacao" class="inputTexto" style="width: 80px;">
-					<option value="+">MAIS</option>					
-					<option value="-">MENOS</option>					
+					<option value="+" '.$arrayOp["+"].'>MAIS</option>					
+					<option value="-" '.$arrayOp["-"].'>MENOS</option>					
 				</select>
 				<input type="text" name="valor2" value="'.$v2.'" class="inputTexto" style="width: 80px;">
 				'.$strRes.'

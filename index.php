e723c872a5a4b1fc332dd3de445cf31545063ded
cleverson <cleverson090@gmail.com>
2015-03-12 11:25:51 -0300

    Interface Gráfica

--- index.php
+++ index.php
@@ -1,3 +1,27 @@
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="style.css">					
+	</head>
+	<body>
+
 <?php
-	echo 'teste';
+	
+	
+	$corpo = '<h1>CALCULADORA<h2>
+			<form method="POST">
+				<input type="text" name="valor1" value="" class="inputTexto" style="width: 80px;">
+				<input type="hidden" name="enviar" value="true">
+				<select name="operacao" class="inputTexto" style="width: 40px;">
+					<option value="+">+</option>
+					<option value="+">-</option>
+					<option value="+">*</option>
+					<option value="+">/</option>
+				</select>
+				<input type="text" name="valor2" value="" class="inputTexto" style="width: 80px;">
+				<br><br>
+				<input type="submit" name="SALVAR" value="CALCULAR" class="botaoEnviar">
+			</form>';
+			echo $corpo;
 ?>
+	</body>
+</html>
